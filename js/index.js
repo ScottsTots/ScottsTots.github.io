b92b817679a164c6a8e3740dacef873f3f039d8f
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-04-16 16:03:24 -0500

    title resp

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var resize = function() {
         $('.section-title').fitText(.8);
+        $('.hero-title').fitText(.5);
     }
     
     $(window).resize(resize);
